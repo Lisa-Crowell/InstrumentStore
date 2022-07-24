262b863e1755b13603ed0e9d45bde28830ad2135
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-24 17:45:10 -0500

    add dbcontext class

---
+++ InstrumentStore/DbContexts/InstrumentStoreDbContext.cs
@@ -0,0 +1,20 @@
+﻿using InstrumentStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InstrumentStore.DbContexts;
+
+/// <summary>
+/// This is a DbContext class for the InstrumentStore.
+/// </summary>
+/// <author>lisa.l.crowell@gmail.com</author>
+public class InstrumentStoreDbContext : DbContext
+{
+    public InstrumentStoreDbContext(DbContextOptions<InstrumentStoreDbContext> options)
+        : base(options)
+    {
+    }
+    
+    public DbSet<Product>? Products { get; set; }
+    public DbSet<Customer>? Customers { get; set; }
+    public DbSet<Order>? Orders { get; set; }
+}
