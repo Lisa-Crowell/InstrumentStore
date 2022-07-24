1b387f1d62cf5dade0153021dba0dcb30227e324
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-24 16:16:17 -0500

    add class customer

---
+++ InstrumentStore/Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace InstrumentStore.Models;
+
+/// <summary>
+/// This is the class that represents a customer.
+/// </summary>
+/// /// <author>lisa.l.crowell@gmail.com</author>
+public class Customer
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string FirstName { get; set; }
+    [Required]
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Zip { get; set; }
+    public DateTime ContactDate { get; set; }
+    [JsonIgnore(Condition = JsonIgnoreCondition.Always)]
+    [InverseProperty(nameof(Order.CustomerDetails))]
+    public ICollection<Order> Orders { get; set; }
+}
