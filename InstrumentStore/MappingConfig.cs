a8ae7fc35224a1ff24851db9f7caf101e99d7b31
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-24 16:39:59 -0500

    add mapping config

---
+++ InstrumentStore/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using InstrumentStore.Models;
+using InstrumentStore.Models.Dto;
+
+namespace InstrumentStore;
+
+public class MappingConfig
+{
+    // method that returns auto mapper configuration
+    public static MapperConfiguration RegisterMaps()
+    {
+        var mappingConfig = new MapperConfiguration(config =>
+        {
+            config.CreateMap<ProductDto, Product>().ReverseMap();
+            config.CreateMap<CustomerDto, Customer>().ReverseMap();
+            config.CreateMap<OrderDto, Order>().ReverseMap();
+        });
+        return mappingConfig;
+    }
+}
