66a2540b046449e8406b7b730eef883c65b486da
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-24 16:12:57 -0500

    add class customer dto

---
+++ InstrumentStore/Models/Dto/CustomerDto.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace InstrumentStore.Models.Dto;
+
+/// <summary>
+/// This is the Dto for the customer model.
+/// </summary>
+/// /// <author>lisa.l.crowell@gmail.com</author>
+public class CustomerDto
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Zip { get; set; }
+    public DateTime ContactDate { get; set; }
+    [JsonIgnore(Condition = JsonIgnoreCondition.Always)]
+    public ICollection<Order> Orders { get; set; }
+}
