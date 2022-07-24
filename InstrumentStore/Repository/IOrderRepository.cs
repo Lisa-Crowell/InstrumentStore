a4702f9523ee98cc2c40a016c7b7b23cc6490441
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-24 15:57:46 -0500

    add interface order repository

---
+++ InstrumentStore/Repository/IOrderRepository.cs
@@ -0,0 +1,18 @@
+﻿using InstrumentStore.Models.Dto;
+
+namespace InstrumentStore.Repository;
+
+/// <summary>
+/// This is the interface for the order repository.
+/// </summary>
+/// /// <author>lisa.l.crowell@gmail.com</author>
+public interface IOrderRepository
+{
+    Task<IEnumerable<OrderDto>> GetOrders();
+    Task<OrderDto> GetOrderById(int id);
+    Task<IEnumerable<OrderDto>> GetByFieldContains(string value);
+    Task<IEnumerable<OrderDto>> GetByDateBetweenDates(string startDate, string endDate);
+    Task<OrderDto> CreateUpdateOrder(OrderDto order);
+    Task<bool> DeleteOrder(int id);
+    Task<IEnumerable<OrderDto>> GetByPriceBetweenPrices(string? start, string? end);
+}
