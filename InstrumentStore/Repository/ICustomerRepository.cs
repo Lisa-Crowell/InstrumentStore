a31e1832a5521bd09c9bd2ca1d173d18513457bf
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-24 15:55:47 -0500

    add interface customer repository

---
+++ InstrumentStore/Repository/ICustomerRepository.cs
@@ -0,0 +1,19 @@
+﻿using InstrumentStore.Models.Dto;
+
+namespace InstrumentStore.Repository;
+
+
+/// <summary>
+/// This is the interface for the customer repository.
+/// </summary>
+/// /// <author>lisa.l.crowell@gmail.com</author>
+public interface ICustomerRepository
+{
+    Task<IEnumerable<CustomerDto>> GetCustomers();
+    Task<CustomerDto> GetCustomerById(int id);
+    Task<IEnumerable<CustomerDto>> GetByFieldContains(string value);
+    Task<CustomerDto> CreateUpdateCustomer(CustomerDto customer);
+    Task<bool> DeleteCustomer(int id);
+    Task<IEnumerable<Tuple<CustomerDto, decimal>>> GetCustomersWithSpentAmount();
+    Task<IEnumerable<CustomerDto>> GetCustomersByInstrument(string instrument);
+}
