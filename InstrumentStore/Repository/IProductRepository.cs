4f20f4d861d528c401b56284135b8649ef123a4a
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-24 15:58:06 -0500

    add interface product repository

---
+++ InstrumentStore/Repository/IProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using InstrumentStore.Models.Dto;
+
+namespace InstrumentStore.Repository;
+
+/// <summary>
+/// This is the interface for the product repository.
+/// </summary>
+/// /// <author>lisa.l.crowell@gmail.com</author>
+public interface IProductRepository
+{
+    Task<IEnumerable<ProductDto>> GetProducts();
+    
+    Task<ProductDto> GetProductById(int id);
+    
+    Task<IEnumerable<ProductDto>> GetByFieldContains(string value);
+    Task<List<Tuple<ProductDto, int>>> GetByDateBetween(string? startDate, string? endDate);
+
+    Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+   
+    Task<bool> DeleteProduct(int id);
+
+    Task<List<ProductDto>> GetByCustomer(int? customer);
+}
