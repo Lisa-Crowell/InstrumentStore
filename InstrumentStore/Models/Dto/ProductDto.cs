183ab84eb0b6a35c37a0d45dd5f436683398ea62
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-24 16:13:30 -0500

    add class product dto

---
+++ InstrumentStore/Models/Dto/ProductDto.cs
@@ -0,0 +1,17 @@
+﻿namespace InstrumentStore.Models.Dto;
+
+/// <summary>
+/// This is the Dto for the product model
+/// </summary>
+/// /// <author>lisa.l.crowell@gmail.com</author>
+public class ProductDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string ModelNumber { get; set; }
+    public decimal RetailPrice { get; set; }
+    public decimal WholeSalePrice { get; set; }
+    public string Category { get; set; }
+    public string Manufacturer { get; set; }
+    public int QuantityInStock { get; set; }
+}
