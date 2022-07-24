aedd34a6a1140a6101bafd3bdffc4cc9cca52bed
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-24 16:17:12 -0500

    add class response dto

---
+++ InstrumentStore/Models/Dto/ResponseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace InstrumentStore.Models.Dto;
+
+/// <summary>
+/// This is the Dto for the response model.
+/// </summary>
+/// /// <author>lisa.l.crowell@gmail.com</author>
+public class ResponseDto
+{
+    public bool IsSuccess { get; set; }
+    public string Message { get; set; }
+    public object Result { get; set; }
+    public List<string> ErrorMessage { get; set; }
+}
